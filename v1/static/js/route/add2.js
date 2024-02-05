9ed99f6c7067f956ed616965bb778983572d120f
oyps <ponconsoft@gmail.com>
2024-02-05 14:04:07 +0800

    ...

--- v1/static/js/route/add2.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-/** `hash = #/add2` */
-var routeEvent = function (route) {
-    alert('你好');
-    console.log(route);
-};
