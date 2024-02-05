d968954cbf3876449bbefe7a54b98eaf53086bcd
oyps <ponconsoft@gmail.com>
2024-02-05 19:38:43 +0800

    ...

--- backend/app.ts
+++ backend/app.ts
@@ -3,8 +3,11 @@ import { getLocalIps } from './util'
 import router from './router'
 
 const app = express()
+
 app.use(router)
+
 const port = process.argv[2] || 6790
+
 app.listen(port, () => {
     getLocalIps().map(ip => console.log(`👉 http://${ip}:${port}`))
 })
--- backend/router.ts
+++ backend/router.ts
@@ -1,7 +1,12 @@
 import { Router } from 'express'
 import login from './route/login'
+import { join } from 'path'
 
 const router = Router()
+
+router.get('/', (_, res) => {
+    res.sendFile('index.html', { root: join(__dirname, '..', '..', 'frontend') })
+})
 
 router.use('/api/login', login)
 
