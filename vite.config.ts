d5e182647d9eefeee7af7ede9b0279dc3ce708a3
oyps <ponconsoft@gmail.com>
2024-02-05 19:53:19 +0800

    ..

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,9 @@ import { defineConfig } from 'vite'
 export default defineConfig({
     root: './frontend',
     server: {
-        host: '0.0.0.0'
+        host: '0.0.0.0',
+        proxy: {
+            '/api': 'http://127.0.0.1:6791'
+        }
     }
 })
